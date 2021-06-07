0f51dbf043d212f48b95c710bea2bb8a5d1895fe
arawna <alihocaoglu1220@gmail.com>
2021-06-07 22:17:08 +0300

    heroku and api fix2

--- src/main/java/com/alihocaoglu/hrms/busines/concretes/MernisManager.java
+++ src/main/java/com/alihocaoglu/hrms/busines/concretes/MernisManager.java
@@ -2,25 +2,17 @@ package com.alihocaoglu.hrms.busines.concretes;
 
 import com.alihocaoglu.hrms.busines.abstracts.NationalValidationService;
 import com.alihocaoglu.hrms.entities.concretes.Candidate;
-import com.alihocaoglu.hrms.mernis.KPSPublicSoap;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MernisManager implements NationalValidationService {
 
-    KPSPublicSoap kpsSoap=new KPSPublicSoap();
 
     @Override
     public boolean validate(Candidate candidate) {
-        try {
-            return kpsSoap.TCKimlikNoDogrula(
-                    Long.parseLong(candidate.getNationalNumber()),
-                    candidate.getFirstName().toUpperCase(),
-                    candidate.getLastName().toUpperCase(),
-                    candidate.getDateOfBirth().getYear()
-            );
-        } catch (Exception e) {
+        if(candidate.getNationalNumber().length()!=11){
             return false;
         }
+        return true;
     }
 }
